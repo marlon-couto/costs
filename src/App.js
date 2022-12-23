5d0a871df119b797689ad8b8b23f9f500a3a7dc4
Marlon Couto <marloncouto.dev@gmail.com>
2022-12-23 02:36:24 -0300

    Adiciona componentes ao diretório "pages"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import { Switch, Route } from 'react-router-dom';
+
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Home from './components/pages/Home';
+
 function App() {
   return (
     <div className="App">
-      <h1>Costs</h1> 
+      <header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Contato</li>
+          </ul>
+        </nav>
+      </header>
+
+      <Switch>
+        <Route
+          exact
+          path="/company"
+          component={Company}
+        />
+
+        <Route
+          exact
+          path="/contact"
+          component={Contact}
+        />
+
+        <Route
+          exact
+          path="/new-project"
+          component={NewProject}
+        />
+
+        <Route
+          exact
+          path="/"
+          component={Home}
+        />
+      </Switch>
+
+      <footer>Footer</footer>
     </div>
   );
 }
