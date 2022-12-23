8d9c3426564aed6d19d7b41b652ed840bde695dc
Marlon Couto <marloncouto.dev@gmail.com>
2022-12-23 09:52:41 -0300

    Adiciona componentes de layout
    Signed-off-by: Marlon Couto <marloncouto.dev@gmail.com>

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contato</Link>
+          </li>
+          <li>
+            <Link to="/company">Empresa</Link>
+          </li>
+          <li>
+            <Link to="/new-project">Novo Projeto</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
